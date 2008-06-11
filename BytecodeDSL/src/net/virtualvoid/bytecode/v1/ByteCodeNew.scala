7aa1d8928f184ae4cd43d071bf5b9df3acda63bc
Johannes Rudolph <johannes_rudolph@gmx.de>
2008-06-11 14:31:09 +0200

    made a cleaner version

---
+++ BytecodeDSL/src/net/virtualvoid/bytecode/v1/ByteCodeNew.scala
@@ -0,0 +1,74 @@
+package net.virtualvoid.bytecode.v1
+
+trait Stack{}
+trait Empty extends Stack{}
+trait S[T,R<:Stack] extends Stack{}
+
+trait Local{}
+trait NoLocal extends Local{}
+trait L[T,R<:Local]{}
+
+/* A Frame state, consisting of stack and locals*/
+trait F[StackT<:Stack,LocalT<:Local]{
+  /* Apply a transformation to this frame state
+   * which changes both stack and local types
+   */
+  def op[Res](func:F[StackT,LocalT]=>Res):Res  
+  /* Apply a transformation to this frame state
+   * which only affects the stack
+   */
+  def sop[Res<:Stack](f:StackT => Res):F[Res,LocalT]
+  /* Apply a transformation which only affects the locals
+   */
+  def lop[Res<:Local](f:LocalT => Res):F[StackT,Res]
+}
+
+trait Ops{
+  type T[FS,TS,FL,TL] = F[FS,FL] => F[TS,TL]
+  type ST[FS,TS,L] = T[FS,TS,L,L]
+  
+  def sop[L,From,To](func: From => To):ST[From,To,L]
+  def lop[X<:Stack,From<:Local,To<:Local](func:From => To):T[X,X,From,To]
+  
+  def iadds[R]:S[int,S[int,R]] => S[int,R]
+  def iadd[LT,R<:Stack]:ST[S[int,S[int,R]],S[int,R],LT] = sop(iadds)
+  
+  def dups[T,R]:S[T,R] => S[T,S[T,R]]
+  def dup[T,R<:Stack,L]:ST[S[T,R],S[T,S[T,R]],L] = sop(dups)
+  
+  def bipushs[R](i:int): R => S[int,R]
+  def bipush[L,R<:Stack](i:int):ST[R,S[int,R],L] = sop(bipushs(i))
+  
+  def pops[T,R]:S[T,R]=>R
+  def pop[T,R<:Stack,L]:ST[S[T,R],R,L] = sop(pops)
+}
+
+object Simulation{
+  type E[LT<:Local] = F[Empty,LT]
+  
+  def test = {
+    val ops:Ops = null
+    val x:F[Empty,NoLocal] = null
+    import ops._    
+    x.op(bipush(12))
+     .op(dup)
+     .op(iadd)
+     .op(pop)
+     .op(bipush(5))
+     .sop(pops)
+    x
+  }
+}
+
+object ShouldNotCompile{
+  val ops:Ops=null
+  val f:F[Empty,NoLocal]=null
+  import ops._
+  def pushFromEmptyStack() {
+    f.op(pop)
+  }
+  def addWhenOnlyOneInteger {
+    f.op(bipush(5))
+     .op(iadd)
+  }
+}
