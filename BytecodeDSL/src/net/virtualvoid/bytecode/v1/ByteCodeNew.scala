315353a09f77923dd6698a3cbaaa223dc62be546
Johannes Rudolph <johannes_rudolph@gmx.de>
2008-08-21 11:48:54 +0200

    move tests into their own file

--- BytecodeDSL/src/net/virtualvoid/bytecode/v1/ByteCodeNew.scala
+++ BytecodeDSL/src/net/virtualvoid/bytecode/v1/ByteCodeNew.scala
@@ -69,23 +69,3 @@ object Simulation{
     x
   }
 }
-
-object ShouldNotCompile{
-  val ops:Ops=null
-  val f:F[Empty,NoLocal]=null
-  import ops._
-  def popFromEmptyStack() {
-    f.op(pop)
-  }
-  def implicitPopFromEmptyStack() {
-    f.pop
-  }
-  def addWhenOnlyOneInteger {
-    f.op(bipush(5))
-     .op(iadd)
-  }
-  def implicitIAddWhenOnlyOneInteger {
-    f.op(bipush(5))
-     .iadd
-  }
-}
