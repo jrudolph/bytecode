da81033ac62c6cb1bed36099b6165d93be8a6761
Johannes Rudolph <johannes_rudolph@gmx.de>
2008-09-04 15:47:49 +0200

    yeah, we have a liftoff

--- BytecodeDSL/src/net/virtualvoid/bytecode/v2/Bytecode.scala
+++ BytecodeDSL/src/net/virtualvoid/bytecode/v2/Bytecode.scala
@@ -4,9 +4,13 @@ object Bytecode{
   trait List
   trait Nil extends List
   object N extends Nil
-  trait Cons[R<:List,T] extends List{
-    def rest:R
-    def top:T
+  case class Cons[+R<:List,+T](rest:R,top:T) extends List
+
+  trait Consable[T<:List]{
+    def **[U](next:U): T**U
+  }
+  implicit def conser[T<:List](t:T) = new Consable[T]{
+    def **[U](next:U): T**U = Cons(t,next)
   }
 
   // define an infix operator shortcut for the cons type
@@ -15,26 +19,32 @@ object Bytecode{
   trait F[ST<:List,LT<:List]{
     def stack:ST
     def locals:LT
-  }
 
+    def iadd_int[R<:List](rest:R,i1:Int,i2:Int):F[R**Int,LT]
+    def bipush(i1:Int):F[ST**Int,LT]
+  }
   trait Int2Stack[ST<:List,LT<:List]{
-    def iadd():F[ST**Int,LT]
-    def imult():F[ST**Int,LT]
+    def i1:Int
+    def i2:Int
+    def rest:ST
+    def frame:F[_,LT]
+    def iadd():F[ST**Int,LT] = frame.iadd_int[ST](rest,i1,i2)
+    def imult():F[ST**Int,LT] = null
   }
   trait OneStack[R<:List,T,LT<:List]{
     def pop():F[R,LT]
   }
-  trait Ops[ST<:List,LT<:List]{
-    def bipush(i:Int):F[ST**Int,LT]
-  }
 
   object Implicits{
-    implicit def ops[ST<:List,LT<:List](f:F[ST,LT]):Ops[ST,LT] = null
-    implicit def int2Stack[R<:List,LT<:List](f:F[R**Int**Int,LT]):Int2Stack[R,LT] = null
+    implicit def int2Stack[R<:List,LT<:List](f:F[R**Int**Int,LT]):Int2Stack[R,LT] = new Int2Stack[R,LT]{
+      val frame = f
+      val stack = f.stack
+      val rest = stack.rest.rest
+      val i1 = stack.rest.top
+      val i2 = stack.top
+    }
     implicit def oneStack[R<:List,LT<:List,T](f:F[R**T,LT]):OneStack[R,T,LT] = null
   }
-
-  val x : Nil**int**String**boolean = null
 
   trait ByteletCompiler{
 	  // compile a piece of code which
@@ -44,11 +54,15 @@ object Bytecode{
   }
 
   object InterpretingCompiler extends ByteletCompiler{
-    case class Stack[R<:List,T](rest:R,top:T) extends Cons[R,T]
-    case class IF[ST<:List,LT<:List](stack:ST,locals:LT) extends F[ST,LT]
+    case class IF[ST<:List,LT<:List](stack:ST,locals:LT) extends F[ST,LT]{
+      def checked[T](x:Any) = x.asInstanceOf[T]
+
+      def iadd_int[R<:List](rest:R,i1:Int,i2:Int):F[R**Int,LT] = IF(rest ** (i1+i2),locals)
+      def bipush(i1:Int):F[ST**Int,LT] = IF(stack ** i1,locals)
+    }
 
     def compile[T,U](code: F[Nil**T,Nil]=>F[Nil**U,_]): T => U =
-      t => code(IF(Stack(N,t),N)).stack.top
+      t => code(IF(N**t,N)).stack.top
   }
 }
 
