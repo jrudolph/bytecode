7eb311dc6bfd53edc299ff2d7dbdd8118453d755
Johannes Rudolph <johannes_rudolph@gmx.de>
2008-09-09 22:14:43 +0200

    some java test class just to look at the eclipse/javac generated bytecodes

--- BytecodeJava/src/net/virtualvoid/bytecode/v2/StringTest.java
+++ BytecodeJava/src/net/virtualvoid/bytecode/v2/StringTest.java
@@ -3,11 +3,42 @@ package net.virtualvoid.bytecode.v2;
 public class StringTest {
 	interface Person{
 		String name();
+		String[] numbers();
+		Iterable<String> numbers_it();
 	}
-	String format(Person p){
+	String format_it(Person p){
 		StringBuilder sb = new StringBuilder();
 		sb.append("Name: ");
 		sb.append(p.name());
+		sb.append(" Tel.: ");
+
+		for(String n:p.numbers_it()){
+			sb.append(n)
+			  .append(",");
+		}
+
+		return sb.toString();
+	}
+
+	String format(Person p){
+		// "Name: #name Tel.: #numbers(,)*"
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("Name: ");
+		sb.append(p.name());
+		sb.append(" Tel.: ");
+		String[] numbers = p.numbers();
+		int length = numbers.length;
+		for(int i=0;i<length;i++){
+			sb.append(numbers[i]);
+			sb.append(",");
+		}
+
+		for(String n:p.numbers()){
+			sb.append(n);
+			sb.append(",");
+		}
+
 		return sb.toString();
 	}
 }
