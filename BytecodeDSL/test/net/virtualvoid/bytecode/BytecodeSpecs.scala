3b84aff5cd2fbc1193c1ccf641b1b2765fe8c083
Johannes Rudolph <johannes_rudolph@gmx.de>
2008-09-18 16:52:11 +0200

    added examples and changed example structure so that compile errors won't stop specs from executing

--- BytecodeDSL/test/net/virtualvoid/bytecode/BytecodeSpecs.scala
+++ BytecodeDSL/test/net/virtualvoid/bytecode/BytecodeSpecs.scala
@@ -95,12 +95,21 @@ import net.virtualvoid.bytecode.v2.Bytecode.Implicits._
     import net.virtualvoid.bytecode.v2.Bytecode._
     import net.virtualvoid.bytecode.v2.Bytecode.Implicits._
     
-    val f1 = compiler.compile(classOf[String])(_.pop.bipush(20).method(Integer.valueOf(_)))
-    "bipush(20)" in {f1("Test") must be_==(20)}
-    val f2 = compiler.compile(classOf[String])(_.method(_.length).method(Integer.valueOf(_)))
-    "method(_.length)" in {f2("Test") must be_==(4)}
-    val f3 = compiler.compile(classOf[java.lang.String])(_.l.store.e.l.load.e.l.load.e.method2(_.concat(_)))
-    "locals + String.concat" in {f3("Test") must be_==("TestTest")}
+    "bipush(20)" in {
+      compiler.compile(classOf[String])(_.pop.bipush(20).method(Integer.valueOf(_)))
+        .apply("Test") must be_==(20)}
+    "method(_.length)" in {
+      compiler.compile(classOf[String])(_.method(_.length).method(Integer.valueOf(_)))
+        .apply("Test") must be_==(4)}
+    "locals + method2" in {
+      compiler.compile(classOf[java.lang.String])(_.l.store.e.l.load.e.l.load.e.method2(_.concat(_)))
+      .apply("Test") must be_==("TestTest")}
+    "iadd" in {
+      compiler.compile(classOf[java.lang.Integer])(_.method(_.intValue).dup.iadd.bipush(3).iadd.method(Integer.valueOf(_)))
+      .apply(12) must be_==(27)}
+    "store int in locals" in {
+      compiler.compile(classOf[java.lang.Integer])(_.method(_.intValue).dup.l.store.e.l.load.e.iadd.method(Integer.valueOf(_)))
+      .apply(12) must be_==(24)}
   }
   
   "Compiler" should {
