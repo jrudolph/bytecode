2166b37cb66550fbe2de219b4e11a2576d036d72
Johannes Rudolph <johannes_rudolph@gmx.de>
2009-08-16 11:14:14 +0200

    update to new specs and scala versions

--- objectformatter/src/test/scala/net/virtualvoid/string/ESParserSpecs.scala
+++ objectformatter/src/test/scala/net/virtualvoid/string/ESParserSpecs.scala
@@ -6,7 +6,7 @@ object ParserSpecs extends Specification {
   import EnhancedStringFormatParser.parse
   import AST._
 
-  "The parser" should parseCorrectly {
+  "The parser" should {
     "'test'" in {"test" must beParsedAs(Literal("test"))}
     "'#prop'" in {"#prop" must beParsedAs(Exp("prop"))}
     "'#{prop}'" in {"#{prop}" must beParsedAs(Exp("prop"))}
@@ -59,8 +59,10 @@ object ParserSpecs extends Specification {
   def expand(e:Exp,sep:String,inner:FormatElement*) = Expand(e,sep,toks(inner:_*))
   def toks(inner:FormatElement*) = FormatElementList(List(inner:_*))
 
+  /* Not working with specs 1.4.3 anymore
   import org.specs.specification.Example
   def parseCorrectly(e: =>Example) = { currentSut.verb += " parse correctly"; e }
+  */
 }
 
 class ParserSpecsTest extends runner.JUnit4(ParserSpecs)
