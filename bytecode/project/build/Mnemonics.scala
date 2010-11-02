7c39eba2082f6e3504120b87eab76f868bd18aca
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-11-02 09:27:32 +0100

    move to specs 1.6.6-SNAPSHOT, since that works flawlessly with sbt

--- bytecode/project/build/Mnemonics.scala
+++ bytecode/project/build/Mnemonics.scala
@@ -1,6 +1,7 @@
 import sbt._
 
 class Mnemonics(info: ProjectInfo) extends DefaultProject(info) {
+  val snapshots = ScalaToolsSnapshots
   val asm = "asm" % "asm" % "3.1"
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.6-SNAPSHOT" % "test"
 }
