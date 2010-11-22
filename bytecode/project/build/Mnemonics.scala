b11c65431cc07a9ce60631fce040891100f860f0
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-11-22 08:11:15 +0100

    update to Scala 2.8.1

--- bytecode/project/build/Mnemonics.scala
+++ bytecode/project/build/Mnemonics.scala
@@ -3,5 +3,5 @@ import sbt._
 class Mnemonics(info: ProjectInfo) extends DefaultProject(info) {
   val snapshots = ScalaToolsSnapshots
   val asm = "asm" % "asm" % "3.1"
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.6-SNAPSHOT" % "test"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test"
 }
