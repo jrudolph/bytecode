55e4a29b1ba0e3f8d86918f585dadd637bdf998b
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-11-24 17:28:58 +0100

    whitespace cleanup and documentation

--- bytecode/src/main/scala/net/virtualvoid/bytecode/List.scala
+++ bytecode/src/main/scala/net/virtualvoid/bytecode/List.scala
@@ -1,21 +1,25 @@
 package net.virtualvoid.bytecode
 
+// our heterogenous list type
 trait List
+// a list is either an empty list Nil...
 trait Nil extends List
-object N extends Nil
-
-case class Cons[+R<:List,+T](rest:R,top:T) extends List
+// or constructed from a rest-list with type R and a top element of type T
+case class Cons[+R <: List, +T](rest: R,top: T) extends List
 
 // define the same for values
-trait Consable[T<:List]{
-  def **[U](next:U): T**U
-}
+object N extends Nil
 
 trait ListShortcuts {
   // define an infix operator shortcut for the cons type
-  type ** [x<:List,y] = Cons[x,y]
-  
-  implicit def conser[T<:List](t:T) = new Consable[T]{
-    def **[U](next:U): T**U = Cons(t,next)
+  type ** [x <: List, y] = Cons[x, y]
+
+  trait Consable[T <: List] {
+    def **[U](next: U): T**U
+  }
+
+  // make every type consable into a heterogenous list 
+  implicit def conser[T <: List](t: T) = new Consable[T] {
+    def **[U](next: U): T**U = Cons(t, next)
   }
 }
