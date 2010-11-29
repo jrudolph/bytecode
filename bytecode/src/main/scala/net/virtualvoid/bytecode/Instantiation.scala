62540303249655d4db805d5c3731a7176d96e6a7
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-11-29 10:59:06 +0100

    implemented two-arg constructors

--- bytecode/src/main/scala/net/virtualvoid/bytecode/Instantiation.scala
+++ bytecode/src/main/scala/net/virtualvoid/bytecode/Instantiation.scala
@@ -12,7 +12,7 @@ case class Ctor1[T, U](override val constructor: jConstructor[_]) extends Constr
   override val numParams = 1
 
   import Bytecode.Instructions._
-  def apply[R <: List](/*arg1: F[Nil] => F[Nil**T]*/): F[R**T] => F[R**U] =
+  def apply[R <: List](): F[R**T] => F[R**U] =
     withLocal { arg1 => f =>
       (f.new_int(constructor.getDeclaringClass) ~
         dup ~
@@ -20,7 +20,20 @@ case class Ctor1[T, U](override val constructor: jConstructor[_]) extends Constr
         .invokeconstructor(this)
     }
 }
+case class Ctor2[T1, T2, U](override val constructor: jConstructor[_]) extends Constructor {
+  override val numParams = 2
+
+  import Bytecode.Instructions._
+  def apply[R <: List](): F[R**T1**T2] => F[R**U] =
+    withLocal { arg2 => _ ~ withLocal { arg1 => f =>
+      (f.new_int(constructor.getDeclaringClass) ~
+        dup ~
+        arg1.load ~ arg2.load)
+        .invokeconstructor(this)
+    }}
+}
 
 trait InstantiationInstructions {
   def ctor1[T, U](c: Code[T => U]): Ctor1[T, U] = Ctor1[T, U](CodeTools.constructorFromTree(c.tree))
+  def ctor2[T1, T2, U](c: Code[(T1, T2) => U]): Ctor2[T1, T2, U] = Ctor2[T1, T2, U](CodeTools.constructorFromTree(c.tree))
 }
