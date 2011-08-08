129ff699038b62c64f4ac8096db55e6438ce4e39
Johannes Rudolph <johannes_rudolph@gmx.de>
2011-08-08 10:17:14 +0200

    sbt-eclipsify plugin

--- bytecode/project/build/Mnemonics.scala
+++ bytecode/project/build/Mnemonics.scala
@@ -1,6 +1,7 @@
 import sbt._
+import de.element34.sbteclipsify._
 
-class Mnemonics(info: ProjectInfo) extends DefaultProject(info) {
+class Mnemonics(info: ProjectInfo) extends DefaultProject(info) with Eclipsify {
   val snapshots = ScalaToolsSnapshots
   val asm = "asm" % "asm" % "3.1"
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.8" % "test"
